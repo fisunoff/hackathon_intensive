63d237c1b405b0a45f24f63ccdd0516aa89796c5
Romver <roma_zverev2002@mail.ru>
2023-03-31 03:29:35 +0300

    Добавлен пользователь в модель Comment

--- comment/models.py
+++ comment/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 from news.models import News
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(News, related_name='comments')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    post = models.ForeignKey(News, on_delete=models.CASCADE)
     text = models.TextField("Описание комментария")
     time_creat = models.DateTimeField("Дата создания", auto_now_add=True)
     time_edit = models.DateTimeField("Дата измения", auto_now=True)
